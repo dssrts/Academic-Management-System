a5ecd1521b0975b4bbb342a5258e9400dc9f0f93
Melissa <mrmmiranda15@gmail.com>
2024-04-01 00:11:54 +0800

    added block to students and removed grades from all views

---
+++ app/Policies/GradePolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Grade;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class GradePolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Grade $grade): bool
+    {
+        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): bool
+    {
+        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user, Grade $grade): bool
+    {
+        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Grade $grade): bool
+    {
+        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Grade $grade): bool
+    {
+        //
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Grade $grade): bool
+    {
+        //
+        return false;
+    }
+}
