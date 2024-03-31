281d0813cc861e60f02cd02532acffa65ea6316a
Melissa <126103639+dssrts@users.noreply.github.com>
2024-03-31 15:10:11 +0800

    made migration for add faculty to subjects nullable

--- database/migrations/2023_12_11_124350_add_faculty_to_subjects.php
+++ database/migrations/2023_12_11_124350_add_faculty_to_subjects.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('subjects', function (Blueprint $table) {
-            $table->string('faculty');
+            $table->string('faculty')->nullable();
         });
     }
 
