a835288ac9601324cdcfd875c2eeb752879fbd1b
Yugi <rafaelagoncillo@gmail.com>
2024-03-31 21:22:28 +0800

    final migrate it plz

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Student extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'student_no',
+        'last_name',
+        'first_name',
+        'middle_name',
+        'biological_sex',
+        'birthdate',
+        'birthdate_city',
+        'religion',
+        'civil_status',
+        'student_type',
+        'registration_status',
+        'year_level',
+        'college_id',
+        'department_id',
+        'academic_year',
+        'permanent_address',
+        'plm'
+    ];
 }
--- database/migrations/2024_03_31_130852_create_students_table.php
+++ database/migrations/2024_03_31_130852_create_students_table.php
@@ -13,6 +13,27 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->integer("student_no");
+            $table->string("last_name");
+            $table->string("first_name");
+            $table->string("middle_name");
+            $table->string("biological_sex");
+            $table->string("birthdate");
+            $table->string("birthdate_city");
+            $table->string("religion");
+            $table->string("civil_status");
+            $table->string("student_type");
+            $table->string("registration_status");
+            $table->integer("year_level");
+            $table->foreignId('college_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('department_id')->constrained()->cascadeOnDelete();
+            $table->string("academic_year");
+            $table->string("permanent_address");
+            $table->string("plm_email");
+            $table->string("personal_email");
+            $table->integer("mobile_no");
+            $table->integer("telephone_no");
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
