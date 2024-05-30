b291eccc62d0377be726451fa67c64eb9ab74e1a
Melissa <mrmmiranda15@gmail.com>
2024-05-31 00:41:26 +0800

    removed filament as providers

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -16,6 +16,7 @@ class AdminPanelProvider extends PanelProvider
     public function panel(Panel $panel): Panel
     {
         return $panel
+        
             ->default()
             ->id('ams')
             ->path('ams')
