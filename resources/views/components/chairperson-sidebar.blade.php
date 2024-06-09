ff627d65bcd013d6bb5e111bc674980f0986126f
Melissa <126103639+dssrts@users.noreply.github.com>
2024-06-09 14:48:13 +0800

    added plm ams at the top

--- resources/views/components/chairperson-sidebar.blade.php
+++ resources/views/components/chairperson-sidebar.blade.php
@@ -14,6 +14,30 @@
         .sidebar-item:hover {
             background-color: rgba(255, 255, 255, 0.1);
         }
+
+        .navbar {
+            background-color: rgba(255, 255, 255, 0.9);
+            padding: 1rem;
+            width: 100%;
+            text-align: center;
+            display: flex;
+            align-items: left;
+            justify-content: left;
+            z-index: 10;
+            position: fixed;
+            top: 0;
+            left: 0;
+        }
+
+        .navbar img {
+            margin-left: 20px;
+            margin-right: 10px;
+        }
+
+        .content {
+            margin-top: 4rem;
+            /* Adjust this value as needed */
+        }
     </style>
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.13.3/dist/cdn.min.js"></script>
     @vite('resources/css/app.css')
@@ -22,10 +46,37 @@
 </head>
 
 <body>
-    <div x-data="{ isOpen: true }" class="flex h-screen " style="color:white">
+
+
+    <nav class="bg-white-10 border-gray-200 dark:bg-gray-900 fixed top-0 left-0 w-full z-50">
+        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                <img src="{{ url('/images/plm-logo.png') }}" class="h-8" alt="PLM Logo" />
+                <span class="self-center font-bold whitespace-nowrap dark:text-blue" style="font-size: 1.5rem;">PLM
+                    AMS</span>
+            </a>
+            <button data-collapse-toggle="navbar-default" type="button"
+                class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                aria-controls="navbar-default" aria-expanded="false">
+                <span class="sr-only">Open main menu</span>
+                <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+                    viewBox="0 0 17 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        d="M1 1h15M1 7h15M1 13h15" />
+                </svg>
+            </button>
+            <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+                <ul
+                    class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div x-data="{ isOpen: true }" class="flex h-screen content" style="color:white">
         <!-- Sidebar -->
         <div :class="isOpen ? 'w-64' : 'w-16'"
-            class="bg-gold h-full transition-all duration-300 overflow-hidden flex flex-col">
+            class="bg-blue h-full transition-all duration-300 overflow-hidden flex flex-col">
             <!-- Toggle Button -->
             <button @click="isOpen = !isOpen" class="p-2 text-white focus:outline-none self-end">
                 <svg x-show="isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
@@ -43,12 +94,13 @@
             <img src="{{ url('images/user.png') }}" alt="user" class="mx-auto mb-2" style="width: 70px; height: 70px;"
                 x-show="isOpen">
 
-            <div class="p-4 text-center bg-gold" x-show="isOpen">
+            <div class="p-4 text-center bg-blue" x-show="isOpen">
                 <div class="bg-white rounded-lg p-4" style="background-color:white">
                     <p class="text-gold"><b>Welcome, </b>{{ $user->name }}!</p>
                     <p class="text-blue font-bold">{{ $departmentName }}</p>
                 </div>
             </div>
+
             <nav class="text-white flex-1">
                 <ul class="space-y-2">
                     <li>
@@ -84,8 +136,6 @@
                     <li>
                         <a href="{{ route('view-appeals') }}"
                             class="sidebar-item flex items-center py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">
-
-
                             <svg class="h-6 w-6" viewBox="0 0 1920 1920" xmlns="http://www.w3.org/2000/svg">
                                 <path
                                     d="m1750.588 1750.118h-1581.176c-31.172 0-56.47-25.412-56.47-56.47v-295.568l210.183 126.155h1273.75l210.184-126.155v295.567c0 31.059-25.299 56.47-56.47 56.47zm-1298.823-338.824v-1242.353h1016.47v1242.353zm1298.823-677.647h-169.412v-677.647h-1242.352v677.647h-169.412c-93.403 0-169.412 76.01-169.412 169.413v790.588c0 93.402 76.01 169.412 169.412 169.412h1581.176c93.403 0 169.412-76.01 169.412-169.412v-790.588c0-93.403-76.01-169.412-169.412-169.412zm-1129.412-244.743h564.706v-112.942h-564.706zm0 677.647h564.706v-112.942h-564.706zm0-338.824h677.648v-112.941h-677.648z"
@@ -97,8 +147,7 @@
                     <li>
                         <a href="{{ route('view-professors') }}"
                             class="sidebar-item flex items-center py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">
-                            <svg fill="#fff" class="h-6 w-6" viewBox="0 0 48 48""
-                                xmlns=" http://www.w3.org/2000/svg">
+                            <svg fill="#fff" class="h-6 w-6" viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg">
                                 <path clip-rule="evenodd" stroke="#fff"
                                     d="m5 4h-1v1 25 1h1 25v-2h-24v-23h29v4h2v-5-1h-1zm31 13c1.6569 0 3-1.3431 3-3s-1.3431-3-3-3-3 1.3431-3 3 1.3431 3 3 3zm-2.0867 2.0088c-.0366 0-.0729.0006-.109.0019h-1.8043c-.4557 0-.8866.2072-1.1713.563l-3.5496 4.437h-5.2791c-.8284 0-1.5.6716-1.5 1.5 0 .8285.6716 1.5 1.5 1.5h6c.4557 0 .8866-.2071 1.1713-.5629l1.9118-2.3898v17.8348c0 .899.7356 1.6346 1.6346 1.6346s1.6346-.7356 1.6346-1.6346v-8.1728h3.2691v8.1728c0 .899.7356 1.6346 1.6346 1.6346s1.6346-.7356 1.6346-1.6346v-12.1486c.899 0 3.675-1.7112 3.675-5.9249 0-3.1759-2.776-4.8105-3.675-4.8105z"
                                     fill="#fff" fill-rule="evenodd" />
@@ -109,16 +158,11 @@
                     <li>
                         <a href="{{ route('view-classes') }}"
                             class="sidebar-item flex items-center py-2.5 px-4 rounded transition duration-200 hover:bg-gray-700">
-
-
-
-
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" fill="none"
                                 stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2z" />
                                 <path d="M22 3h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z" />
                             </svg>
-
                             <span class="ml-4" x-show="isOpen">View Classes</span>
                         </a>
                     </li>
@@ -143,6 +187,9 @@
                     <!-- Add more sidebar options here -->
                 </ul>
             </nav>
+        </div>
+        <div class="flex-1">
+            <!-- Main content goes here -->
         </div>
     </div>
 </body>
